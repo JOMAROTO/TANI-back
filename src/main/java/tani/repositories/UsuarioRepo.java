b9424f5928acb3d3362ec67be608bb9ef6f9c654
JOMAROTO <jomaroto.08@hotmail.com>
2025-03-08 10:45:33 -0500

    cuarto commit

--- src/main/java/tani/repositories/UsuarioRepo.java
+++ src/main/java/tani/repositories/UsuarioRepo.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface UsuarioRepo extends JpaRepository<Usuario, Integer> {
-
     Optional<Usuario> findByCorreo(String correo);
     Optional<Usuario> findByTipoUsuario(TIPO_USUARIO tipo_usuario);
 }
